d134c1026baae491a415d90f83c6e0bde018007b
Tarun Kumar <tfortarun89@gmail.com>
2026-04-22 15:35:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,5 +23,5 @@ def run_etl():
         return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
